8e711fe397e7bd13ac1f35fd6546789f1ce7dd86
mt3262 <mt3262@drexel.edu>
2021-05-08 20:18:09 -0400

    Assignment 6
    Refactor/Cleanup

--- src/test/java/CommandValidatorTest.java
+++ src/test/java/CommandValidatorTest.java
@@ -7,17 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CommandValidatorTest {
 
     private static final String ID = "12345678";
-    private static final String ID2 = "87654321";
-    private static final String ID3 = "32423454";
     private static final String CHECKING = "checking";
     private static final String SAVINGS = "savings";
     private static final String CD = "cd";
     private static final double APR = 0.6332423;
     private static final double BALANCE = 0;
-    private static final double DEPOSIT = 500;
-    private static final double WITHDRAWAL = 250;
     private static final double CDBALANCE = 2000;
-    private static final double LARGEWITHDRAWAL = 2500;
 
     CommandValidator commandValidator;
     Bank bank;
